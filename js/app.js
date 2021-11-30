f85123a5370ead3ab70e1e36b320040f03d3770a
edin420 <77352546+edin420@users.noreply.github.com>
2021-11-30 18:17:14 +0100

    added gallery section

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
-  var btnToTop = $(".btnToTop");
-  var arrow = $(".arrow");
-  var navToggleBtn = $(".nav-toggle");
-  var responsive_menu = $(".nav");
+$(document).ready(function () {
+  var btnToTop = $('.btnToTop');
+  var arrow = $('.arrow');
+  var navToggleBtn = $('.nav-toggle');
+  var responsive_menu = $('.nav');
 
   // ON SCROLL HIDE "ARROW DOWN" - SHOW BTN "SCROLL TO TOP"
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     if ($(this).scrollTop() > 50) {
       btnToTop.fadeIn();
       arrow.fadeOut();
@@ -17,19 +17,19 @@ $(document).ready(function() {
   });
 
   // NAVBAR HIDE AND SHOW ON CLICK
-  navToggleBtn.on("click", function(e) {
+  navToggleBtn.on('click', function (e) {
     e.preventDefault();
     responsive_menu.slideToggle();
   });
 
-  $(window).resize(function() {
+  $(window).resize(function () {
     var obtener_ancho = $(this).width();
     if (obtener_ancho > 865) {
-      responsive_menu.removeAttr("style");
+      responsive_menu.removeAttr('style');
     }
   });
 
-  $(".nav__link").on("click", function(e) {
+  $('.nav__link').on('click', function (e) {
     var obtener_ancho = $(window).width();
     if (obtener_ancho < 865) {
       responsive_menu.slideToggle();
@@ -37,5 +37,5 @@ $(document).ready(function() {
   });
 
   // GET FULL YEAR SET IN FOOTER
-  $(".fullYear").text(new Date().getFullYear());
+  $('.fullYear').text(new Date().getFullYear());
 });
