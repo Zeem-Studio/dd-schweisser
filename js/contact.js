11f5c27614580edd37911eedaa31377b5d7447f8
edin420 <77352546+edin420@users.noreply.github.com>
2021-12-03 18:43:09 +0100

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  var submitBtn = $(".submitBtn");
+  var submitBtn = $('.submitBtn');
 
   // CONTACT FORM DATA VALIDATION AND SEND DATA API
   submitBtn.click(function sendContact() {
@@ -7,29 +7,25 @@ $(document).ready(function () {
     valid = validateContact();
     if (valid) {
       $.ajax({
-        url: "https://formsubmit.co/ajax/info.dgm.2015@gmail.com",
-        method: "POST",
+        url: 'https://formsubmit.co/ajax/info@ddschweisser.de',
+        method: 'POST',
         data: {
-          name: $("#userName").val(),
-          email: $("#userEmail").val(),
-          reason: $("#contactReason").val(),
-          message: $("#content").val(),
+          name: $('#userName').val(),
+          email: $('#userEmail').val(),
+          reason: $('#contactReason').val(),
+          message: $('#content').val(),
         },
-        dataType: "json",
+        dataType: 'json',
 
         success: function () {
-          $("#mail-status").html(
-            "<p class='mailSuccess'>Message sent. We will respond as soon as possible.</p>"
-          );
-          $(".submitBtn").fadeOut(100);
-          $("#mail-status").fadeIn(500);
+          $('#mail-status').html("<p class='mailSuccess'>Nachricht gesendet. Wir werden so schnell wie möglich antworten.</p>");
+          $('.submitBtn').fadeOut(100);
+          $('#mail-status').fadeIn(500);
         },
         error: function () {
-          $("#mail-status").html(
-            "<p class='mailError'>Error. Please contact us directly via email info.dgm.2015@gmail.com(</p>"
-          );
-          $(".submitBtn").fadeOut(100);
-          $("#mail-status").fadeIn(500);
+          $('#mail-status').html("<p class='mailError'>Error. Bitte kontaktieren Sie uns direkt über email info@ddschweisser.de(</p>");
+          $('.submitBtn').fadeOut(100);
+          $('#mail-status').fadeIn(500);
         },
       });
     }
@@ -37,24 +33,19 @@ $(document).ready(function () {
   // Validate eneterd data in contact form
   function validateContact() {
     var valid = true;
-    $(".info").html("");
-    if (
-      !$("#userName").val() ||
-      !$("#userEmail").val() ||
-      !$("#contactReason").val() ||
-      !$("#content").val()
-    ) {
-      $("#mail-status").html("<p class='mailError'>All fields required!</p>");
-      $("#mail-status").fadeIn(500);
+    $('.info').html('');
+    if (!$('#userName').val() || !$('#userEmail').val() || !$('#contactReason').val() || !$('#content').val()) {
+      $('#mail-status').html("<p class='mailError'>Alle Felder benötigt!</p>");
+      $('#mail-status').fadeIn(500);
       valid = false;
     }
     if (
-      !$("#userEmail")
+      !$('#userEmail')
         .val()
         .match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)
     ) {
-      $("#mail-status").html("<p class='mailError'>Email invalid!</p>");
-      $("#mail-status").fadeIn(500);
+      $('#mail-status').html("<p class='mailError'>E-Mail ungültig!</p>");
+      $('#mail-status').fadeIn(500);
       valid = false;
     }
     return valid;
